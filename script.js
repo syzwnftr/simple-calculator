8a9a39dc017ff524cf91e764d9162f607f6a11e7
Syazwan Fitri <syazfitry69@gmail.com>
2021-11-27 10:53:10 +0800

    Added function to display numbers,operators on display when clicked

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const btnOne = document.querySelector('.btn-1');
 const currentOperandText = document.querySelector('.current-operand');
+const allBtn = document.querySelectorAll('.btn')
 
-btnOne.addEventListener('click', () => {
-    currentOperandText.innerText = btnOne.innerText;
-});
+// btnOne.addEventListener('click', () => {
+//     currentOperandText.innerText = btnOne.innerText;
+// });
+
+allBtn.forEach((btn) => {
+   btn.addEventListener('click', () => {
+    currentOperandText.innerText = btn.innerText;
+   });
+})
+
